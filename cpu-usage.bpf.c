47a42d0e3884744e740cd3cf8d50d3c60bcceae2
Daniel Schneider <daniel.schneider@hotellistat.de>
2022-12-18 15:16:01 +0100

    feat: plot

--- cpu-usage.bpf.c
+++ cpu-usage.bpf.c
@@ -1,8 +1,8 @@
-#include <uapi/linux/ptrace.h>
+#include <linux/kernel.h>
+#include <linux/ptrace.h>
 #include <uapi/linux/bpf.h>
-#include <linux/sched.h>
-#include <linux/perf_event.h>
-
+#include <uapi/linux/bpf_perf_event.h>
+#include <uapi/linux/perf_event.h>
 // struct data_t
 // {
 //     u32 pid;
@@ -10,31 +10,35 @@
 //     char comm[TASK_COMM_LEN];
 // };
 
-BPF_HASH(usage, int, int);
-BPF_HASH(last, int, int);
+BPF_HASH(usage, int, long);
+BPF_HASH(last, int, long);
 
 // BPF_HASH(last_usage, int, int);
 
 int measure_cpu_usage(struct bpf_perf_event_data *ctx)
 {
 
-    struct bpf_perf_event_value data =
-        {};
+    struct bpf_perf_event_value value_buf;
 
-    bpf_perf_prog_read_value(ctx, &data, sizeof(data));
+    long result = bpf_perf_prog_read_value(ctx, (void *)&value_buf, sizeof(struct bpf_perf_event_value));
 
-    int pid = bpf_get_current_pid_tgid() >> 32;
+    if (result < 0)
+    {
+        return 0;
+    }
 
-    bpf_trace_printk("%d", &data);
+    int pid = bpf_get_current_pid_tgid();
 
-    int time = bpf_ktime_get_ns();
+    // bpf_trace_printk("%d", &data);
+
+    // int time = bpf_ktime_get_ns();
 
     // if (time < 0)
     // {
     //     return 0;
     // }
 
-    usage.update(&pid, &data);
+    usage.update(&pid, &value_buf);
 
     // u64 ts, *tsp, delta;
 
--- cpu-usage.py
+++ cpu-usage.py
@@ -1,10 +1,21 @@
 import bcc
 import sys
 import time
+from tabulate import tabulate
+import os
+import matplotlib.pyplot as plt
+from textwrap import wrap
 # pid= int(sys.argv[1])
 
+pid = os.getpid()
+
+
+import subprocess
+def get_pname(id):
+    p = subprocess.Popen(["ps -o cmd= {}".format(id)], stdout=subprocess.PIPE, shell=True)
+    return str(p.communicate()[0])
 # Load the eBPF program into the kernel
-bpf_text = open("./cpu-usage.c", "r").read()
+bpf_text = open("./cpu-usage.bpf.c", "r").read()
 b = bcc.BPF(text=bpf_text)
 
 # Attach the eBPF function to the sched_switch tracepoint
@@ -23,13 +34,35 @@ b.attach_perf_event(
 # b['usage'].open_perf_buffer(print_event)
 
 while 1:
-    result = b.trace_fields()
+    # result = b.trace_fields()
 
     usage = b.get_table("usage")
 
-    print(usage.items(), result)
+    # print(tabulate(usage.items()))
+
+
+    data = list(map(lambda datapoint: (str(datapoint[0].value), datapoint[1].value), usage.items()))
+
+    data.sort(key=lambda item: item[1])
+
+    x, y = zip(*data)
+
+    print(x, y)
+
+    # print(data)
+    # labels = list(map(lambda process: str(process[0].value), data))
+
+    # [ '\n'.join(wrap(l, 20)) for l in labels ]
+
+    # values = list(map(lambda process: process[1].value, data))
+    # print(labels, values)
+    plt.bar(x, y)
+    plt.savefig('plot.png')
+    plt.clf()
+
     # for k, v in sorted(usage.items(), key=lambda usage: usage[1].value):
     #     print("%10d \"%s\"" % (v.value, k.c.encode('string-escape')))
     # b.perf_buffer_poll()
-    time.sleep(1)
+    time.sleep(2)
     # print(b['usage'])
+
