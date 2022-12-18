0035ffb8ec650587d6323d26bc5a7aea36cb4687
Daniel Schneider <daniel.schneider@hotellistat.de>
2022-12-18 12:48:07 +0100

    feat: fixes

--- cpu-usage.c
+++ cpu-usage.c
@@ -2,6 +2,7 @@
 #include <uapi/linux/bpf.h>
 #include <linux/sched.h>
 #include <linux/perf_event.h>
+
 // struct data_t
 // {
 //     u32 pid;
@@ -10,17 +11,55 @@
 // };
 
 BPF_HASH(usage, int, int);
-BPF_HASH(last_usage, int, int);
+BPF_HASH(last, int, int);
+
+// BPF_HASH(last_usage, int, int);
 
 int measure_cpu_usage(struct bpf_perf_event_data *ctx)
 {
-    int pid = bpf_get_current_pid_tgid();
 
-    bpf_perf_prog_read_value(ctx, struct bpf_perf_event_value * buf, u32 buf_size)
+    struct bpf_perf_event_value data =
+        {};
 
-        int cpu_usage = 1000;
+    bpf_perf_prog_read_value(ctx, &data, sizeof(data));
 
-    usage.update(&pid, &cpu_usage);
+    int pid = bpf_get_current_pid_tgid() >> 32;
+
+    bpf_trace_printk("%d", &data);
+
+    int time = bpf_ktime_get_ns();
+
+    // if (time < 0)
+    // {
+    //     return 0;
+    // }
+
+    usage.update(&pid, &data);
+
+    // u64 ts, *tsp, delta;
+
+    // // attempt to read stored timestamp
+    // u64 tsp = last.lookup(&pid);
+    // if (tsp != NULL)
+    // {
+    //     delta = bpf_ktime_get_ns() - *tsp;
+
+    //     usage.update(&pid, &delta);
+
+    //     if (delta < 1000000000)
+    //     {
+    //         // output if time is less than 1 second
+    //         bpf_trace_printk("%d\\n", delta / 1000000);
+    //     }
+    //     last.delete(&key);
+    // }
+
+    // if ()
+    // pid_hist.update(&pid, &time);
+
+    // bpf_perf_prog_read_value(ctx, struct bpf_perf_event_value * buf, u32 buf_size);
+
+    // int cpu_usage = 1000;
 
     return 0;
 }
--- cpu-usage.py
+++ cpu-usage.py
@@ -6,10 +6,11 @@ import time
 # Load the eBPF program into the kernel
 bpf_text = open("./cpu-usage.c", "r").read()
 b = bcc.BPF(text=bpf_text)
+
 # Attach the eBPF function to the sched_switch tracepoint
 b.attach_perf_event(
-    ev_type=bcc.PerfType.SOFTWARE,
-    ev_config=bcc.PerfSWConfig.CPU_CLOCK,
+    ev_type=bcc.PerfType.HARDWARE,
+    ev_config=bcc.PerfHWConfig.CPU_CYCLES,
     fn_name="measure_cpu_usage",
     sample_freq=10
 )
@@ -22,9 +23,11 @@ b.attach_perf_event(
 # b['usage'].open_perf_buffer(print_event)
 
 while 1:
+    result = b.trace_fields()
+
     usage = b.get_table("usage")
 
-    print(usage.items())
+    print(usage.items(), result)
     # for k, v in sorted(usage.items(), key=lambda usage: usage[1].value):
     #     print("%10d \"%s\"" % (v.value, k.c.encode('string-escape')))
     # b.perf_buffer_poll()
