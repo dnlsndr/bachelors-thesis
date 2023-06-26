b160ca5ee1d305382247201645c879fa4b87d9d2
Daniel Schneider <daniel.schneider@eramux.com>
2023-06-26 15:37:55 +0200

    feat: improvements

--- cpu-usage.bpf.c
+++ cpu-usage.bpf.c
@@ -1,16 +1,20 @@
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 
-#define MINBLOCK_US 1
+#define MINBLOCK_US 1000000
 
 struct key_t
 {
-    char name[TASK_COMM_LEN];
-    int pid;
+    u64 pid;
+};
+struct cmd_t
+{
+    char comm[TASK_COMM_LEN];
 };
 BPF_HASH(counts, struct key_t);
 BPF_HASH(start, u32);
-BPF_STACK_TRACE(stack_traces, 10240);
+BPF_HASH(pid_mapping, u64, struct cmd_t);
+// BPF_STACK_TRACE(stack_traces, 10240);
 
 int oncpu(struct pt_regs *ctx, struct task_struct *prev)
 {
@@ -18,12 +22,12 @@ int oncpu(struct pt_regs *ctx, struct task_struct *prev)
     u64 ts, *tsp;
 
     // record previous thread sleep time
-    if (!(prev->flags & PF_KTHREAD))
-    {
-        pid = prev->pid;
-        ts = bpf_ktime_get_ns();
-        start.update(&pid, &ts);
-    }
+    // if (!(prev->flags & PF_KTHREAD))
+    // {
+    pid = prev->pid;
+    ts = bpf_ktime_get_ns();
+    start.update(&pid, &ts);
+    // }
 
     // calculate current thread's delta time
     pid = bpf_get_current_pid_tgid();
@@ -33,22 +37,26 @@ int oncpu(struct pt_regs *ctx, struct task_struct *prev)
     u64 delta = bpf_ktime_get_ns() - *tsp;
     start.delete(&pid);
     delta = delta / 1000;
+
     if (delta < MINBLOCK_US)
+    {
         return 0;
+    }
 
     // create map key
     u64 zero = 0, *val;
     struct key_t key = {};
     int stack_flags = 0;
 
-    /*
-    if (!(prev->flags & PF_KTHREAD))
-      stack_flags |= BPF_F_USER_STACK;
-    */
-
-    bpf_get_current_comm(&key.name, sizeof(key.name));
+    u64 user_space_pid = bpf_get_current_pid_tgid();
     // key.stack_id = stack_traces.get_stackid(ctx, stack_flags);
-    key.pid = prev->pid;
+    key.pid = user_space_pid;
+
+    struct cmd_t cmd = {};
+
+    bpf_get_current_comm(&cmd.comm, sizeof(cmd.comm));
+
+    pid_mapping.update(&user_space_pid, &cmd);
 
     val = counts.lookup_or_try_init(&key, &zero);
     if (val)
--- cpu-usage.py
+++ cpu-usage.py
@@ -1,34 +1,51 @@
 from bcc import BPF
 import os
 import matplotlib.pyplot as plt
+import time
 
 def main():
     bpf = BPF(src_file="cpu-usage.bpf.c")
     bpf.attach_kprobe(event="finish_task_switch.isra.0", fn_name="oncpu")
-    # bpf.attach_tracepoint("sched_process_exit", "offcpu")
+    # bpf.attach_tracepoint("sched_process_exit", "oncpu")
 
-    print("eBPF program loaded and attached to Docker cgroup. Press Ctrl+C to exit.")
+    print("eBPF program loaded and attached probe. Press Ctrl+C to exit.")
 
-    while 1:
-        try:
-            items = bpf["counts"].items()
-
-            for (k, v) in items:
-                print(k.name, k.pid, v.value)
+    time.sleep(4)
 
 
-            fig = plt.figure(figsize = (5, 10))
+    items = list(filter( lambda x: (x[1].value > 0) ,bpf["counts"].items()))
 
-            plt.barh([str(k.pid) for (k, v) in items], [v.value for (k, v) in items], tick_label = [k.name for (k, v) in items])
 
-            plt.savefig('plot.png')
+    pid_mapping = bpf["pid_mapping"].items()
 
-        except KeyboardInterrupt:
-            print("Exiting...")
-            exit()
-        # (task, pid, cpu, flags, ts, msg) = bpf.trace_fields()
-        # (pid, comm, cmd, us) = bpf.ksyms[msg[4:]].split()
-        # print(f"{ts}: {comm} {pid} {us}")
+    pid_dict = {}
+
+    for (k, v) in pid_mapping:
+        pid_dict[k.value] = v.comm
+
+    result = {}
+
+    for (k, v) in items:
+        result[k.pid] = {
+            "value": v.value,
+            "pid": k.pid >> 32,
+            "name": pid_dict[k.pid]
+        }
+
+    print(result.items())
+
+    items = sorted(items, key=lambda x: x[1].value)
+
+    fig = plt.figure(figsize = (6, 10))
+    ax = fig.add_subplot(111)
+
+    ax.barh([str(k) for k, v in result.items()], [v["value"] for k, v in result.items()], tick_label = [v["name"] for k, v in result.items()])
+
+    plt.tight_layout()
+    plt.savefig('plot.png')
+    plt.close()
+
+
 
 
 if __name__ == "__main__":
