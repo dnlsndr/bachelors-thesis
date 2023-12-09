ac34ebbb92db858941e957af43bef6ae60e36591
Daniel Schneider <daniel.schneider@eramux.com>
2023-12-09 13:17:32 +0100

    feat: correct

--- cpu-usage.py
+++ cpu-usage.py
@@ -1,3 +1,5 @@
+import psutil  # Import psutil
+
 from bcc import BPF
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -15,8 +17,18 @@ last_update = {}  # Dictionary to track last update time for each PID
 fig, ax = plt.subplots()
 
 
+def get_process_name(pid):
+    """Get process name by PID."""
+    try:
+        return psutil.Process(pid).name()
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        return "Unknown"
+
+
 def update(frame):
     current_time = time.time()
+    bar_data = {}  # Dictionary to store current data for bar chart
+
     for k, v in b["cpu_time"].items():
         pid = k.value
         cpu_time = v.value
@@ -32,6 +44,9 @@ def update(frame):
             data[pid].append(cpu_time)
             last_update[pid] = current_time
 
+        # Store the most recent data for bar chart
+        bar_data[pid] = data[pid][-1]
+
     # Remove inactive PIDs
     inactive_pids = [pid for pid, last_time in last_update.items(
     ) if current_time - last_time > inactive_threshold]
@@ -41,16 +56,28 @@ def update(frame):
 
     b["cpu_time"].clear()
 
+    # Sort bar_data by CPU time and keep the top 20
+    sorted_data = sorted(
+        bar_data.items(), key=lambda x: x[1], reverse=True)[:20]
+    pids, cpu_times = zip(*sorted_data)
+
     ax.clear()
-    for pid, timings in data.items():
-        ax.plot(timings, label=f"PID {pid}")
+    y_pos = range(len(pids))  # Create categorical bands for each PID
+    ax.barh(y_pos, cpu_times, tick_label=[
+            f"{get_process_name(pid)} (PID {pid})" for pid in pids])
 
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("CPU Time (nanoseconds)")
-    plt.title("CPU Time per Process (Sliding Window)")
-    plt.legend()
+    plt.xlabel("CPU Time (nanoseconds)")
+    plt.ylabel("Processes")
+    plt.title("Top 20 Processes by Current CPU Time")
+
+    # Adjust legend position
+    ax.legend(loc='upper center', bbox_to_anchor=(
+        0.5, -0.05), shadow=True, ncol=2)
+
+    # Adjust layout
+    plt.tight_layout()
 
 
-ani = FuncAnimation(fig, update, interval=100)  # Update every second
+ani = FuncAnimation(fig, update, interval=10000)  # Update every second
 
 plt.show()
--- probes/oncpu.bpf.c
+++ probes/oncpu.bpf.c
@@ -9,36 +9,32 @@ int oncpu(struct tracepoint__sched__sched_switch *args)
 
     u32 prev_pid = args->prev_pid;
     u32 next_pid = args->next_pid;
+    u64 *prev_start_time, curr_time, time_diff;
 
-    u64 ts, *tsp, delta, *timep;
+    // Get current time
+    curr_time = bpf_ktime_get_ns();
 
-    // Check if process is switching out
-    if (prev_pid != 0)
+    // Exclude the idle process
+    // if (prev_pid == 0 || next_pid == 0)
+    // {
+    //     return 0;
+    // }
+
+    // Update time for previous process
+    prev_start_time = start.lookup(&prev_pid);
+    if (prev_start_time != 0)
     {
-        ts = bpf_ktime_get_ns();
-        start.update(&prev_pid, &ts);
+        time_diff = curr_time - *prev_start_time;
+        u64 *prev_cpu_time = cpu_time.lookup_or_init(&prev_pid, &time_diff);
+        if (prev_cpu_time)
+        {
+            *prev_cpu_time += time_diff;
+        }
+        start.delete(&prev_pid);
     }
 
-    // Process switching in
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    tsp = start.lookup(&pid);
-    if (tsp != 0)
-    {
-        delta = bpf_ktime_get_ns() - *tsp;
-        timep = cpu_time.lookup(&pid);
-        if (timep != 0)
-        {
-            *timep += delta;
-        }
-        else
-        {
-            cpu_time.update(&pid, &delta);
-        }
-        start.delete(&pid);
-    }
-
-    // ts = bpf_ktime_get_ns();
-    // start.update(&pid, &ts);
+    // Record start time for next process
+    start.update(&next_pid, &curr_time);
 
     return 0;
 }
