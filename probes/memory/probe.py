af36c802aa10f8d6aab5d58f65267b930bc69365
Daniel Schneider <daniel.schneider@eramux.com>
2024-01-12 16:54:03 +0100

    memory fixes

--- probes/memory/probe.py
+++ probes/memory/probe.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
 import re
+import ctypes
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, './ebpf.c')
@@ -30,6 +31,13 @@ def init(inputConfig):
     global g
     g = Gauge('ebpf_memory_utilization', 'memory utilization', ['pid', 'name'])
 
+    for proc in psutil.process_iter():
+        if proc.pid not in initial_memory_usage:
+            initial_memory_usage[proc.pid] = read_initial_memory_usage(
+                proc.pid)
+            g.labels(pid=proc.pid, name=proc.name()).set(
+                initial_memory_usage[proc.pid])
+
 
 def get_process_name(pid):
     try:
@@ -45,22 +53,44 @@ def read_initial_memory_usage(pid):
             # Extract the RSS value `(Resident Set Size)
             rss_values = re.findall(r'VmSize:\s+(\d+)', content)
             rss_values = [int(val) for val in rss_values]
-            return sum(rss_values) * 1024  # Convert to bytes
+            return sum(rss_values) * 1024
     except FileNotFoundError:
         return 0
 
 
 def update():
-    data = b["usage"].items()
+    data = b["usage"]
     b["usage"].clear()
 
-    # g.clear()
+    g.clear()
 
-    for k, v in data:
-        if k.value == 0:
-            continue
+    for k, v in data.items():
+        # try:
+        #     process = psutil.Process(k.value)
+        # except psutil.NoSuchProcess:
+        #     g.remove(pid=k.value)
+        #     continue
+        # if not process.is_running():
+        #     g.remove(pid=k.value)
+        #     continue
         if k.value not in initial_memory_usage:
             initial_memory_usage[k.value] = read_initial_memory_usage(k.value)
-        name = get_process_name(k.value)
-        total_memory_usage = initial_memory_usage[k.value] + v.value
-        g.labels(pid=k.value, name=name).set(total_memory_usage)
+
+    for pid, value in initial_memory_usage.items():
+        # try catch
+        try:
+            process = psutil.Process(pid)
+        except psutil.NoSuchProcess:
+            continue
+
+        name = get_process_name(pid)
+
+        # add data[pid].value if its not None
+        final = value + \
+            (data[ctypes.c_uint32(pid)].value if ctypes.c_uint32(pid) in data else 0)
+
+        # if ctypes.c_uint32(pid) in data:
+        #     print("data", pid, data[ctypes.c_uint32(pid)].value)
+        g.labels(pid=pid, name=name).set(final)
+
+    # loop through all processes and prefill the initial memory usage
